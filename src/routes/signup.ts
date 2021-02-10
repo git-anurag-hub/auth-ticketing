32000b731fd30da80771b841088a010bd9412748
Anurag Gupta <aguptaking@gmail.com>
2021-02-10 19:58:33 +0530

    User

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -14,7 +14,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -23,8 +23,15 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("Creating User...");
-    res.send({});
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.send({});
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+    res.status(201).send(user);
   }
 );
 
